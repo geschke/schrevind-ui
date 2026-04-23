754f0bc5a93025d755c26152ce420b642b48ee03
Ralf Geschke <ralf@kuerbis.org>
2026-04-23 20:41:10 +0200

    add current group context to route key

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
+import { computed } from "vue";
+import { useUserAuthStore } from "@/stores/userauth";
 
+const storeUserAuth = useUserAuthStore();
+const activeGroupID = computed(() => storeUserAuth.activeGroupID);
 </script>
 
 <template>
         <router-view v-slot="{ Component, route }">
-        <component :is="Component" :key="route.fullPath"></component>
+        <component :is="Component" :key="`${route.fullPath}-${activeGroupID ?? 0}`"></component>
       </router-view>
   
 </template>
